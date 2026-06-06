15aead9b9985a6046339ba38ec8117ba3bd0b23b
harshitmishra <harshitmishra1514@gmail.com>
2026-06-06 14:47:51 +0530

    policy repository created

---
+++ src/main/java/com/harshit/monocept/repository/PolicyRepository.java
@@ -0,0 +1,18 @@
+package com.harshit.monocept.repository;
+
+import com.harshit.monocept.entity.Policy;
+import com.harshit.monocept.enums.PolicyStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PolicyRepository extends JpaRepository<Policy, Long> {
+	Optional<Policy> findByPolicyNumber(String policyNumber);
+
+	Page<Policy> findByCustomerId(Long customerId, Pageable pageable);
+
+	Page<Policy> findByStatus(PolicyStatus status, Pageable pageable);
+}
