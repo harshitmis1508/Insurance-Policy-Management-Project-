f5a13e2358580a4582f2766579e842b7b60c3327
HA <haagarwal67@gmail.com>
2026-06-07 16:03:12 +0530

    Create Agent Request

--- src/main/java/com/harshit/monocept/dto/request/CreateAgentRequest.java
+++ src/main/java/com/harshit/monocept/dto/request/CreateAgentRequest.java
@@ -1,9 +1,13 @@
 package com.harshit.monocept.dto.request;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// SRS FR-USER-004: Admin agent account banaye
 @Getter
 @Setter
 @NoArgsConstructor
