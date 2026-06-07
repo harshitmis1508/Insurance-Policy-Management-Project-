6e22d2078c244a708a171cff18f12807b896434a
HA <haagarwal67@gmail.com>
2026-06-07 16:03:27 +0530

    Customer request

--- src/main/java/com/harshit/monocept/dto/request/CustomerRequest.java
+++ src/main/java/com/harshit/monocept/dto/request/CustomerRequest.java
@@ -1,34 +1,42 @@
 package com.harshit.monocept.dto.request;
 
-import jakarta.validation.constraints.*;
-import lombok.*;
 import java.time.LocalDate;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerRequest {
 
-    // SRS: CUS validation - dateOfBirth past date honi chahiye
-    @NotNull(message = "Date of birth is required")
-    @Past(message = "Date of birth must be a past date")
-    private LocalDate dateOfBirth;
+	@NotNull(message = "Date of birth is required")
+	@Past(message = "Date of birth must be a past date")
+	private LocalDate dateOfBirth;
 
-    @NotBlank(message = "Address is required")
-    private String address;
+	@NotBlank(message = "Address is required")
+	private String address;
 
-    @NotBlank(message = "City is required")
-    private String city;
+	@NotBlank(message = "City is required")
+	private String city;
 
-    @NotBlank(message = "State is required")
-    private String state;
+	@NotBlank(message = "State is required")
+	private String state;
 
-    // SRS: PIN code valid hona chahiye
-    @NotBlank(message = "PIN code is required")
-    @Pattern(regexp = "^[1-9][0-9]{5}$", message = "Invalid PIN code")
-    private String pinCode;
+	@NotBlank(message = "PIN code is required")
+	@Pattern(regexp = "^[1-9][0-9]{5}$", message = "Invalid PIN code")
+	private String pinCode;
 
-    @NotBlank(message = "Nominee name is required")
-    private String nomineeName;
+	@NotBlank(message = "Nominee name is required")
+	private String nomineeName;
 
-    @NotBlank(message = "Nominee relation is required")
-    private String nomineeRelation;
+	@NotBlank(message = "Nominee relation is required")
+	private String nomineeRelation;
 }
